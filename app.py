70b7472c87ce5e77232e4bf2c1bf9b45405439ab
suku <sukumarreddyp06@gmail.com>
2026-06-22 14:52:02 +0530

    Fix Render Port

--- app.py
+++ app.py
@@ -565,4 +565,5 @@ def create_admin_table():
 
     return "Admin table created"   
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
