40c331940b0b8f7d8b9df2db2f7fd3220db35cab
suku <sukumarreddyp06@gmail.com>
2026-06-22 15:41:39 +0530

    Added edit employee page

--- app.py
+++ app.py
@@ -566,4 +566,4 @@ def create_admin_table():
     return "Admin table created"   
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
